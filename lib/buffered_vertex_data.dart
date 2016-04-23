a489c05bac8f78c4b5a9d164a062d1cb8339dabf
R.S. Schermer <roland0507@gmail.com>
2016-04-23 11:06:06 +0200

    Don't expert vertex from buffered_vertex_data

--- lib/buffered_vertex_data.dart
+++ lib/buffered_vertex_data.dart
@@ -7,8 +7,6 @@ import 'dart:typed_data';
 import 'math.dart';
 import 'vertex.dart';
 
-export 'vertex.dart';
-
 part 'src/buffered_vertex_data/attribute_data_frame.dart';
 part 'src/buffered_vertex_data/buffered_vertex_collection.dart';
 part 'src/buffered_vertex_data/vertex_attribute.dart';
