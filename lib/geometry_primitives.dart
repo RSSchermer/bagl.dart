01522cb78bf84a33f1bd971f52c83aaa89ff9683
R.S. Schermer <roland0507@gmail.com>
2016-04-23 13:22:08 +0200

    Move primitives into separate library

---
+++ lib/geometry_primitives.dart
@@ -0,0 +1,59 @@
+/// Primitives for describing vertex geometry.
+library geometry_primitives;
+
+import 'vertex.dart';
+
+/// Point vertex geometry primitive.
+///
+/// A point is defined by a single [vertex].
+///
+/// See also [Line] and [Triangle].
+class Point {
+  /// The vertex for the point.
+  final Vertex vertex;
+
+  /// Instantiates a new [Point].
+  Point(this.vertex);
+}
+
+/// Line vertex geometry primitive.
+///
+/// A line is defined by two vertices: the [start] vertex and the [end] vertex.
+/// Typically describes a line segment between these vertices. More complex
+/// paths are typically described by multiple lines that share vertices (e.g.
+/// the first line's ending vertex is the second line's starting vertex).
+///
+/// See also [Point] and [Triangle].
+class Line {
+  /// The line's starting vertex.
+  final Vertex start;
+
+  /// The line's ending vertex.
+  final Vertex end;
+
+  /// Instantiates a new [Line].
+  Line(this.start, this.end);
+}
+
+/// Triangle vertex geometry primitive.
+///
+/// A triangle is defined by three vertices: vertex [a], vertex [b] and vertex
+/// [c]. Typically describes a surface between these vertices. More complex
+/// surfaces are typically described by multiple triangles that share vertices
+/// (e.g. vertices [b] and [c] on the first triangle are the same vertices as
+/// vertices [a] and [b] on the second triangle).
+///
+/// See also [Point] and [Line].
+class Triangle {
+  /// The triangle's first vertex.
+  final Vertex a;
+
+  /// The triangle's second vertex.
+  final Vertex b;
+
+  /// The triangle's third vertex.
+  final Vertex c;
+
+  /// Instantiates a new [Triangle].
+  Triangle(this.a, this.b, this.c);
+}
--- lib/topology.dart
+++ lib/topology.dart
@@ -1,16 +1,17 @@
-/// Data structures for the description of renderable vertex topologies.
+/// Data structures for describing geometries with buffered vertex data and
+/// buffered index data.
 library topology;
 
 import 'dart:collection';
 import 'dart:typed_data';
 
 import 'buffered_vertex_data.dart';
+import 'geometry_primitives.dart';
 
 part 'src/topology/line_loop.dart';
 part 'src/topology/line_strip.dart';
 part 'src/topology/lines.dart';
 part 'src/topology/points.dart';
-part 'src/topology/primitives.dart';
 part 'src/topology/triangle_fan.dart';
 part 'src/topology/triangle_strip.dart';
 part 'src/topology/triangles.dart';
