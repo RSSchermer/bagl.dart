0772409f1e1709a983e5c0a0c6e244a74a1a215c
R.S. Schermer <roland0507@gmail.com>
2016-05-02 13:01:55 +0200

    Rename triangle_test to draw_triangles_test

---
+++ test/web_gl/draw_triangles_test.dart
@@ -0,0 +1,63 @@
+@TestOn('browser')
+
+import 'dart:html';
+
+import 'package:test/test.dart';
+import 'package:bagl/bagl.dart';
+import 'package:bagl/web_gl.dart';
+
+void main() {
+  group('WebGL', () {
+    group('draw Triangles', () {
+      var canvas = document.querySelector('#main_canvas');
+      var context = new RenderingContext(canvas, preserveDrawingBuffer: true);
+
+      var vertexShaderSource = """
+        attribute vec2 position;
+        attribute vec3 color;
+
+        varying vec3 vColor;
+
+        void main(void) {
+          gl_Position = vec4(position, 0., 1.);
+          vColor = color;
+        }
+      """;
+
+      var fragmentShaderSource = """
+        precision mediump float;
+
+        varying vec3 vColor;
+
+        void main(void) {
+          gl_FragColor = vec4(vColor, 1.);
+        }
+      """;
+
+      var program = new Program(context, vertexShaderSource, fragmentShaderSource);
+
+      var vertices = new VertexArray([
+        new Vertex({
+          'position': new Vector2(0.0, 0.5),
+          'color': new Vector3(1.0, 0.0, 0.0)
+        }),
+        new Vertex({
+          'position': new Vector2(-0.5, -0.5),
+          'color': new Vector3(0.0, 1.0, 0.0)
+        }),
+        new Vertex({
+          'position': new Vector2(0.5, -0.5),
+          'color': new Vector3(0.0, 0.0, 1.0)
+        })
+      ]);
+
+      var triangles = new Triangles(vertices);
+
+      program.drawTriangles(triangles);
+
+      test('draws the correct frame', () {
+        expect(canvas.toDataUrl(), equals(document.querySelector('#expected').src));
+      });
+    });
+  });
+}
