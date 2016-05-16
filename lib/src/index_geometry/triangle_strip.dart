7bffb74e03a24fb43c0536adcae47e368d246b26
R.S. Schermer <roland0507@gmail.com>
2016-05-16 23:46:25 +0200

    Clean up doc

--- lib/src/index_geometry/triangle_strip.dart
+++ lib/src/index_geometry/triangle_strip.dart
@@ -19,14 +19,14 @@ part of index_geometry;
 /// - Triangle `5`: (`a`:`v6`, `b`:`v5`, `c`:`v7`)
 /// - Triangle `6`: ...
 ///
-/// Note that each triangle shares 2 vertices with the preceding triangle:
-///
-///     v1---v3---v5---v7
-///     |\   |\   |\   |
-///     | \  | \  | \  |
-///     |  \ |  \ |  \ |
-///     |   \|   \|   \|
-///     v0---v2---v4---v6
+///     // The connectedness of the vertices in a triangle strip:
+///     //
+///     // v1---v3---v5---v7
+///     // |\   |\   |\   |
+///     // | \  | \  | \  |
+///     // |  \ |  \ |  \ |
+///     // |   \|   \|   \|
+///     // v0---v2---v4---v6
 ///
 /// See also [Triangles] and [TriangleFan].
 class TriangleStrip extends IterableBase<TriangleStripTriangleView>
