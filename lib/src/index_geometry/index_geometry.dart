7ced7282e62c65fc0789736ce713076c41711734
R.S. Schermer <roland0507@gmail.com>
2016-05-22 22:59:48 +0200

    Improve doc

--- lib/src/index_geometry/index_geometry.dart
+++ lib/src/index_geometry/index_geometry.dart
@@ -1,7 +1,7 @@
 part of index_geometry;
 
-/// Enumerates the ways in which a sequence of vertices can be combined into
-/// a sequence of geometry primitives.
+/// Enumerates the ways in which a sequence of vertices can be turned into a
+/// sequence of geometry primitives.
 enum Topology {
   points,
   lines,
