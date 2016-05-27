7ffc6062e5ce3afc41ff62f81259426ea4fa5143
R.S. Schermer <roland0507@gmail.com>
2016-05-27 21:21:29 +0200

    Add webgl test for index geometry with an offset and count

---
+++ test/web_gl/index_geometry_offset_count_test.dart
@@ -0,0 +1,75 @@
+@TestOn('browser')
+
+import 'dart:html';
+
+import 'package:test/test.dart';
+import 'package:bagl/bagl.dart';
+import 'package:bagl/web_gl.dart';
+
+void main() {
+  group('WebGL', () {
+    group('draw index geometry with an offset and a count', () {
+      var canvas = document.querySelector('#main_canvas');
+      var context = RenderingContext.forCanvas(canvas, preserveDrawingBuffer: true);
+
+      var vertexShaderSource = """
+        attribute vec2 position;
+        attribute vec3 color;
+
+        varying vec3 vColor;
+
+        void main(void) {
+          gl_Position = vec4(position, 0., 1.);
+          vColor = color;
+        }
+      """;
+
+      var fragmentShaderSource = """
+        precision mediump float;
+
+        varying vec3 vColor;
+
+        void main(void) {
+          gl_FragColor = vec4(vColor, 1.);
+        }
+      """;
+
+      var program = new Program.fromSource(context, vertexShaderSource, fragmentShaderSource);
+
+      var vertices = new VertexArray([
+        new Vertex({
+          'position': new Vector2(-0.5, 0.5),
+          'color': new Vector3(1.0, 0.0, 0.0)
+        }),
+        new Vertex({
+          'position': new Vector2(-1.0, -0.5),
+          'color': new Vector3(0.0, 1.0, 0.0)
+        }),
+        new Vertex({
+          'position': new Vector2(-0.5, -0.5),
+          'color': new Vector3(0.0, 0.0, 1.0)
+        }),
+        new Vertex({
+          'position': new Vector2(0.5, -0.5),
+          'color': new Vector3(1.0, 0.0, 0.0)
+        }),
+        new Vertex({
+          'position': new Vector2(0.0, 0.5),
+          'color': new Vector3(0.0, 1.0, 0.0)
+        }),
+        new Vertex({
+          'position': new Vector2(0.0, 0.5),
+          'color': new Vector3(0.0, 0.0, 1.0)
+        })
+      ]);
+
+      var triangles = new Triangles(vertices, new IndexList.incrementing(6), 2, 3);
+
+      context.draw(triangles, program);
+
+      test('draws the correct frame', () {
+        expect(canvas.toDataUrl(), equals(document.querySelector('#expected').src));
+      });
+    });
+  });
+}
